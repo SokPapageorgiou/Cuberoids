9be64fcd6a75fa53dc0ce3eb93ca335419459d79
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2022-10-25 20:41:02 +0200

    Add PawnShooter

---
+++ Assets/Scripts/Systems/PlayerControl/PawnControl/PawnShooter.cs
@@ -0,0 +1,21 @@
+﻿using Systems.PlayerControl.PawnControl.Interfaces;
+using UnityEngine;
+
+namespace Systems.PlayerControl.PawnControl
+{
+    public class PawnShooter : MonoBehaviour, IPawnWatcher
+    {
+        private IShootable _shootable;
+        private bool _isTriggerPulled;
+
+        private void FixedUpdate()
+        {
+            if(_isTriggerPulled) _shootable?.Fire();
+        }
+
+        public void UpdatePawn(Rigidbody2D pawn) 
+            => _shootable = pawn.GetComponent(typeof(IShootable)) as IShootable;
+        
+        private bool InvertTrigger() => _isTriggerPulled = !_isTriggerPulled;
+    }
+}
