70677e6dde082a2b9d97f482f453d232b077219e
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2022-10-26 15:20:34 +0200

    Add Gun to Ship

--- Assets/Scripts/Characters/SpaceShip/SS_Shoot.cs
+++ Assets/Scripts/Characters/SpaceShip/SS_Shoot.cs
@@ -1,19 +1,25 @@
-using System;
+using System.Numerics;
 using Systems.ObjectPool;
 using Systems.PlayerControl.PawnControl.Interfaces;
 using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
 
 namespace Characters.SpaceShip
 {
     public class SS_Shoot : MonoBehaviour , IShootable
     {
-        [SerializeField] private float fireRate;
-
+        [SerializeField] private float shotsPerSecond;
+        [SerializeField] private Transform gun;
+       
         private Pool _pool;
 
         private void Awake() => _pool = FindObjectOfType<Pool>();
-        
-        public void Fire() => _pool.GetInstance(PoolEntry.Bullet);
-        
+
+        public void Fire()
+        {
+           var bullet= _pool.GetInstance(PoolEntry.Bullet);
+
+           bullet.transform.position = gun.position;
+        }
     }
 }
