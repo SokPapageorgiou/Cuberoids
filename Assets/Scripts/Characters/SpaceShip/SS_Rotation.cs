4ceba929d4a4b0dd6e313756b84b4d913c928d2c
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2022-10-26 10:43:58 +0200

    Add SS_Rotation

---
+++ Assets/Scripts/Characters/SpaceShip/SS_Rotation.cs
@@ -0,0 +1,18 @@
+using System;
+using Systems.PlayerControl.PawnControl.Interfaces;
+using UnityEngine;
+
+namespace Characters.SpaceShip
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class SS_Rotation : MonoBehaviour, IRotetable
+    {
+        [SerializeField] private float magnifier;
+        
+        private Rigidbody2D _rigidbody2D;
+
+        private void Awake() => _rigidbody2D = GetComponent<Rigidbody2D>();
+
+        public void Rotate(float userInput) => _rigidbody2D.rotation += magnifier * userInput;
+    }
+}
