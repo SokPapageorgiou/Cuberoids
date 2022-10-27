bdfa49d6aea6b20c0c4f016b755e5bf9581b64ab
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2022-10-27 15:15:24 +0200

    HotFix StageWarp

--- Assets/Scripts/Entities/StageWarp/Warp.cs
+++ Assets/Scripts/Entities/StageWarp/Warp.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 namespace Entities.StageWarp
@@ -6,6 +5,31 @@ namespace Entities.StageWarp
     [RequireComponent(typeof(BoxCollider2D))]
     public class Warp : MonoBehaviour
     {
-        private void OnTriggerExit2D(Collider2D other) => other.transform.position *= -1;
+        private BoxCollider2D _boxCollider2D;
+
+        private void Awake() => _boxCollider2D = GetComponent<BoxCollider2D>();
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            var otherPosition = other.transform.position;
+            float newPosX = WarpInOneAxis(_boxCollider2D.size.x / 2, otherPosition.x);
+            float newPosY = WarpInOneAxis(_boxCollider2D.size.y / 2, otherPosition.y);
+
+            other.transform.position = new Vector3(newPosX, newPosY);
+        } 
+
+        public float WarpInOneAxis(float range, float number)
+        {
+            if (IsOutOfRange(range, number)) number = InvertNumber(number);
+            return number;
+        }
+
+        private bool IsOutOfRange(float range, float number)
+        {
+            if (number >= -range && number <= range) return false;
+            return true;
+        }
+        
+        private float InvertNumber(float number) => number * -1;
     }
 }
