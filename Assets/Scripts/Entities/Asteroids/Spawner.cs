16c68c88223b06942a900746dd100a8a5b3be99b
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2022-10-27 15:39:34 +0200

    Add StatusRandomizer

--- Assets/Scripts/Entities/Asteroids/Spawner.cs
+++ Assets/Scripts/Entities/Asteroids/Spawner.cs
@@ -1,6 +1,5 @@
 using Systems.ObjectPool;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
 namespace Entities.Asteroids
 {
@@ -14,51 +13,22 @@ namespace Entities.Asteroids
         [Header("Spawn area")] 
         [SerializeField] private float width;
         [SerializeField] private float height;
-        [SerializeField] private float distanceToCenter;
+        [SerializeField] private float distanceFromCenter;
 
         private Pool _pool;
-
-        private void Awake() =>  _pool = FindObjectOfType<Pool>();
+        private readonly StatusRandomizer _randomizer = new();
+        
+        private void Awake() => _pool = FindObjectOfType<Pool>();
 
         private void Start()
         {
             for (int i = 0; i < amountOnStart; i++)
             {
                 var instance = _pool.GetInstance(PoolEntry.Asteroid);
-                instance.transform.position = SetRandomLocation();
-                instance.AddRelativeForce(SetRandomVector2());
-                instance.angularVelocity = SetRandomAngularVelocity();
+                instance.transform.position = _randomizer.SetRandomLocation(distanceFromCenter, width, height);
+                instance.AddRelativeForce(_randomizer.SetRandomVector2(maxForceMagnitude));
+                instance.angularVelocity = _randomizer.SetRandomAngularVelocity(maxAngularSpeed);
             }
         }
-
-        private Vector2 SetRandomLocation()
-        {
-            float x = Random.Range(distanceToCenter, width);
-            float y = Random.Range(distanceToCenter, height);
-            
-            x = RandomInvertDirection(x);
-            y = RandomInvertDirection(y);
-            
-            return new Vector2(x, y);
-        }
-
-        private float RandomInvertDirection(float number)
-        {
-            if (Random.value > 0.5f) number *= -1;
-            return number;
-        }
-
-        private Vector2 SetRandomVector2()
-        {
-            float x = SetRandomNormalized();
-            float y = SetRandomNormalized();
-            
-            float magnitude = Random.Range(0, maxForceMagnitude);
-            
-            return new Vector2(x,y).normalized * magnitude;
-        }
-
-        private float SetRandomNormalized() => Random.Range(-1, 1);
-        private float SetRandomAngularVelocity() => SetRandomNormalized() * maxAngularSpeed;
     }
 }
---
+++ Assets/Scripts/Entities/Asteroids/StatusRandomizer.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace Entities.Asteroids
+{
+    public class StatusRandomizer
+    {
+        public Vector2 SetRandomLocation(float distanceFromCenter, float width, float height)
+        {
+            float x = Random.Range(distanceFromCenter, width);
+            float y = Random.Range(distanceFromCenter, height);
+            
+            x = RandomInvertDirection(x);
+            y = RandomInvertDirection(y);
+            
+            return new Vector2(x, y);
+        }
+
+        public Vector2 SetRandomVector2(float maxForceMagnitude)
+        {
+            float x = SetRandomNormalized();
+            float y = SetRandomNormalized();
+            
+            float magnitude = Random.Range(0, maxForceMagnitude);
+            
+            return new Vector2(x,y).normalized * magnitude;
+        }
+
+        public float SetRandomAngularVelocity(float maxAngularSpeed) => SetRandomNormalized() * maxAngularSpeed;
+
+        private float RandomInvertDirection(float number)
+        {
+            if (Random.value > 0.5f) number *= -1;
+            return number;
+        }
+
+        private float SetRandomNormalized() => Random.Range(-1, 1);
+    }
+}
