4194f7bd1e90cd5d36bbbd9d1f2034fe597fd4e3
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2022-10-27 16:19:31 +0200

    Refactor SO_UnityEvent

--- Assets/Scripts/Entities/Utilities/SO_UnityEvent.cs
+++ Assets/Scripts/Entities/Utilities/SO_UnityEvent.cs
@@ -6,10 +6,10 @@ namespace Entities.Utilities
     [CreateAssetMenu(fileName = "NewUnityEvent", menuName = "SO_Events/UnityEvent")]
     public class SO_UnityEvent : ScriptableObject
     {
-        private UnityEvent<Vector2> _event;
+        private UnityEvent<Transform> _event;
 
-        public void Subscribe(UnityAction<Vector2> action) => _event.AddListener(action);
-        public void Unsubscribe(UnityAction<Vector2> action) => _event.RemoveListener(action);
-        public void Invoke(Vector2 data) => _event.Invoke(data);
+        public void Subscribe(UnityAction<Transform> action) => _event.AddListener(action);
+        public void Unsubscribe(UnityAction<Transform> action) => _event.RemoveListener(action);
+        public void Invoke(Transform transform) => _event.Invoke(transform);
     }
 }
